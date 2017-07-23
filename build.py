50bcce5fef89469d48aa38b183731c07776f9240
rishabh-github <rishabh-github@users.noreply.github.com>
2017-07-23 13:28:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
 
-def equal_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+def equal_operator(array1, array2):
+    ds1 = pd.Series(array1)
+    ds2 = pd.Series(array2)
+    ds = ds1 == ds2
+    return ds
 
 
-def greater_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+def greater_than_operator(array1, array2):
+    ds1 = pd.Series(array1)
+    ds2 = pd.Series(array2)
+    ds = ds1 > ds2
+    return ds
 
 
-def less_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+def less_than_operator(array1, array2):
+    ds1 = pd.Series(array1)
+    ds2 = pd.Series(array2)
+    ds = ds1 < ds2
+    return ds
